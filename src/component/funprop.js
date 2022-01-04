c7e9b5d4fe91c55ca949547276ef01f43e54bdb8
PushpaSatvika <92777679+PushpaSatvika@users.noreply.github.com>
2022-01-04 17:53:25 +0530

    practiced the State and prop and components are commited by satvika

---
+++ src/component/funprop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const funprop = (props) => {
+    return (
+        <div>
+       <h3>this is sample functional component</h3>,
+       <h3>hello {props.name} Welcome to REACT JS VISUAL CODE</h3>
+       </div>
+    );
+}
+export default funprop;
